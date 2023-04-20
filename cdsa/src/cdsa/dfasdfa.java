8c35c5a24afdba194ff5d9bf9ac5d174fd543450
DOHMISOL <doms0406@gmail.com>
2023-04-20 21:36:43 +0900

    z
    z

---
+++ cdsa/src/cdsa/dfasdfa.java
@@ -0,0 +1,34 @@
+package cdsa;
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Scanner;
+
+
+public class dfasdfa {
+	public static void main(String[] args) {
+		
+		 Queue<Integer> minheap = new PriorityQueue<Integer>();
+	      minheap.offer(6);
+	      minheap.offer(5);
+	      minheap.offer(4);
+	      minheap.offer(2);
+	      minheap.offer(3);
+	      System.out.println(minheap.toString());
+	      
+//	      System.out.println("픽 :" + minheap.peek());
+//	      System.out.println(minheap.toString());
+	      
+//	      System.out.println("폴 :" + minheap.poll());
+//	      System.out.println(minheap.toString());
+//	      
+//	      System.out.println("리무브 :" + minheap.remove());
+//	      System.out.println(minheap.toString());
+//	      
+//	      boolean check = minheap.isEmpty();//minheap이 비었으면 true 
+//	      
+//	      minheap.clear();
+//	      System.out.println(minheap.toString());
+//		
+	}
+}
